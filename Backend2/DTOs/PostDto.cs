e23649bfa2edaaa4c8e40a4dbc0becbd624fdb99
LuisOG <orlando.98.gonzales@gmail.com>
2024-04-10 16:12:45 -0500

    Basic DTO structure

---
+++ Backend2/DTOs/PostDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Backend2.DTOs
+{
+    public class PostDto
+    {
+        public int id {  get; set; }
+        public int UserId { get; set; }
+        public string? Title { get; set; }
+        public string? body { get; set; }
+    }
+}
