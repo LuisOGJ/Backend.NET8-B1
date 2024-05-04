55c7a3981132fa4b0de36162c181a6748e095fa5
LuisOG <orlando.98.gonzales@gmail.com>
2024-05-04 15:14:09 -0500

    READ ALL/ READ BY ID

---
+++ Backend2/Controllers/BeerController.cs
@@ -0,0 +1,46 @@
+﻿using Backend2.DTOs;
+using Backend2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BeerController : ControllerBase
+    {
+        public StoreContext _context;
+
+        public BeerController(StoreContext context)
+        {
+            _context = context; 
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<BeerDto>> Get() => await _context.Beer.Select(b => new BeerDto 
+            { 
+                Id = b.BeerID,
+                Name = b.Name,
+                Alcohol = b.Alcohol,
+                BrandID = b.BrandID,
+            }).ToListAsync();
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BeerDto>> GetById(int id) {
+            var beer = await _context.Beer.FindAsync(id);
+            if (beer == null) { 
+                return NotFound();
+            }
+            var beerDto = new BeerDto
+            {
+                Id = beer.BeerID,
+                Name = beer.Name,
+                Alcohol = beer.Alcohol,
+                BrandID = beer.BrandID,
+            };
+            return Ok(beerDto);
+        }
+    }
+}
