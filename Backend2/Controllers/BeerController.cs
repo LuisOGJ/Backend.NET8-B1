a31de70089e9bb3a09da235a25079f01d42e4ff7
LuisOG <orlando.98.gonzales@gmail.com>
2024-05-04 15:54:53 -0500

    CREATE using DTO, assing información at header

--- Backend2/Controllers/BeerController.cs
+++ Backend2/Controllers/BeerController.cs
@@ -14,23 +14,23 @@ namespace Backend2.Controllers
 
         public BeerController(StoreContext context)
         {
-            _context = context; 
+            _context = context;
         }
 
         [HttpGet]
-        public async Task<IEnumerable<BeerDto>> Get() => await _context.Beer.Select(b => new BeerDto 
-            { 
-                Id = b.BeerID,
-                Name = b.Name,
-                Alcohol = b.Alcohol,
-                BrandID = b.BrandID,
-            }).ToListAsync();
+        public async Task<IEnumerable<BeerDto>> Get() => await _context.Beer.Select(b => new BeerDto
+        {
+            Id = b.BeerID,
+            Name = b.Name,
+            Alcohol = b.Alcohol,
+            BrandID = b.BrandID,
+        }).ToListAsync();
 
 
         [HttpGet("{id}")]
         public async Task<ActionResult<BeerDto>> GetById(int id) {
             var beer = await _context.Beer.FindAsync(id);
-            if (beer == null) { 
+            if (beer == null) {
                 return NotFound();
             }
             var beerDto = new BeerDto
@@ -42,5 +42,29 @@ namespace Backend2.Controllers
             };
             return Ok(beerDto);
         }
+
+        [HttpPost]
+        public async Task<ActionResult<BeerDto>> Add(BeerInsertDto beerInsertDto) {
+            var beer = new Beer()
+            {
+                Name = beerInsertDto.Name,
+                BrandID = beerInsertDto.BrandID,
+                Alcohol = beerInsertDto.Alcohol
+            };
+
+            await _context.Beer.AddAsync(beer); // agrega los datos faltantes a beer y se puede usar lineas abajo
+            await _context.SaveChangesAsync(); // para que se representen los datos en la db
+
+            var beerDto = new BeerDto()
+            {
+                Id = beer.BeerID,
+                Name = beer.Name,
+                BrandID = beer.BrandID,
+                Alcohol = beer.Alcohol,
+            };
+
+            return CreatedAtAction(nameof(GetById), new { id = beer.BeerID}, beerDto);
+        }
+
     }
 }
